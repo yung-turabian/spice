ba7798cdb9d6a2a5fa99f101f04515035d2b11f8
Henry Wandover <henrywandover@gmail.com>
2025-03-18 21:07:32 -0400

    first commit

---
+++ spice.ml
@@ -0,0 +1,53 @@
+module Ease =
+struct
+    let fst3 (x, _, _) = x
+    let snd3 (_, y, _) = y
+    let thd3 (_, _, z) = z
+end
+
+module IO =
+struct
+    (** [readdir_stripped handle].
+        Requires: [handle <> NULL]. 
+        Returns: A unix directory that is not `.` or `..`*)
+    let rec readdir_stripped handle = 
+      let entry = Unix.readdir handle in 
+      if entry <> "." && entry <> ".." 
+        then entry 
+      else readdir_stripped handle
+
+    let argv_debug _ =
+      for i = 0 to Array.length Sys.argv - 1 do
+        Printf.printf "[%i] %s\n" i Sys.argv.(i)
+      done
+
+
+    let print_head lst =
+      match lst with
+      | [] -> print_endline "The list is empty."
+      | head :: _ -> print_endline head 
+
+
+    let is_hidden file =
+        let prefix = "." in
+        if String.starts_with ~prefix file then true
+        else false
+end
+
+
+module Math =
+struct
+    (** [fact n] is [n!].
+        Requires: [n >= 0]. *)
+        let rec fact n = if n = 0 then 1 else n * fact (n - 1)
+end
+
+
+module String =
+struct
+    (** Brief: Check for substring within a string. *)
+    let strstr s1 s2 =
+      let re = Str.regexp_string s2 in
+      try ignore (Str.search_forward re s1 0); true
+      with Not_found -> false
+end
